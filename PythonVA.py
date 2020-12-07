fe5f0a1353ce98e0726d1388fc77fff756fd4d4e
Mahwish Khan <mahwish.k99@gmail.com>
2020-12-06 19:27:18 -0500

    Added wolframalpha and wikipedia api functionality w/o gui

--- PythonVA.py
+++ PythonVA.py
@@ -1,1 +1,24 @@
 # PythonVA App
+import wx
+import wolframalpha
+import wikipedia
+
+app_id = "2KX35Q-66JJ87KHEX"
+client = wolframalpha.Client(app_id)
+
+# lang = "en" // lang = "fr" 
+# wikipedia.set_lang(lang)
+
+while(True):
+    inp = input("Q: ")
+    try:
+        # wolframalpha
+        res = client.query(inp)
+        answer = next(res.results).text
+        print(answer)
+
+    except: 
+        # wikipedia 
+        print(wikipedia.summary(inp, sentences=3))
+
+
